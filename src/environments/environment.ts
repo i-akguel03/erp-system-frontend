854ad0569ca3b3e515f22d12f2f52dbaf99664e7
i-akguel03 <i.akguel@outlook.com>
2025-08-24 16:43:09 +0200

    Stop tracking environments

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-//   apiBaseUrl: 'http://localhost:8080'
-  apiBaseUrl: 'https://erp-system-backend-yo8w.onrender.com'
-};
--- src/environments/environments.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://erp-backend.example.com/api'
-};
