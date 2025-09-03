dc2afee1e73146d6b223523b4ad7c69efdd4bd00
i-akguel03 <i.akguel@outlook.com>
2025-09-03 19:24:53 +0200

    login und register angepasst

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
 import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
 import { BackendCheckComponent } from './shared/backend-check/backend-check/backend-check';
+import { AuthService } from './auth/services/auth';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,17 @@ import { BackendCheckComponent } from './shared/backend-check/backend-check/back
     NavbarComponent,
     CardModule,
     CommonModule,
-    BackendCheckComponent // ✅ Registrieren
+    BackendCheckComponent
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('erp-system-frontend');
-  protected backendReady = signal(false); // ✅ als Signal
+  protected backendReady = signal(false);
+
+  constructor(private auth: AuthService) {}
+
+  // Computed Signal: ist User eingeloggt?
+  protected readonly isLoggedIn = computed(() => this.auth.isAuthenticated());
 }
